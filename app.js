246d7d2f618a0c2ee1f1a7534ca481ea84074b0f
Martina Pereyra <1martinapereyra@gmail.com>
2022-10-02 19:40:45 -0300

    cambio funciones de ataque, curar, verificarGanador, y calcular Heridas

--- app.js
+++ app.js
@@ -17,34 +17,95 @@ new Vue({
         },
 
         empezarPartida: function () {
-             this.hayUnaPartidaEnJuego=true;
+            this.turnos=[];
+            this.hayUnaPartidaEnJuego=true;
+            this.saludJugador = 100;
+            this.saludMonstruo = 100;
         },
         atacar: function () {
+            let bajaPuntos = this.calcularHeridas(this.rangoAtaque);
+            this.saludMonstruo -= bajaPuntos;
+            this.turnos.unshift({
+                esJugador:true,
+                text:"El jugador golpea al monstruo por " + bajaPuntos
+            });
+            
+            if(this.verificarGanador()){
+                return;
+            }
+
+            this.ataqueDelMonstruo();
         },
 
         ataqueEspecial: function () {
+            let bajaPuntos = this.calcularHeridas(this.rangoAtaqueEspecial)
+            this.saludMonstruo -= bajaPuntos;
+            this.turnos.unshift({
+                esJugador:false,
+                text:"El Jugador golpea al monstruo por " + bajaPuntos
+            });
+
+            if(this.verificarGanador()){
+                return;
+            }
+
+            this.ataqueDelMonstruo(); 
         },
 
         curar: function () {
+            if(this.saludJugador <= 90){
+                this.saludJugador +=10;
+                this.turnos.unshift({
+                    esJugador:true,
+                    text:"El Jugador incrementar su energía por 10 puntos"
+                });
+            }else{
+                this.saludJugador = 100;
+                this.turnos.unshift({
+                    esJugador:true,
+                    text:"El Jugador recuperea su salud"
+                });
+            }
+
+            this.ataqueDelMonstruo();
         },
 
         registrarEvento(evento) {
         },
-        
+
         terminarPartida: function () {
             this.hayUnaPartidaEnJuego=false;
-            this.saludJugador = 100;
-            this.saludMonstruo = 100;
         },
 
         ataqueDelMonstruo: function () {
+            let bajaPuntos = this.calcularHeridas(this.rangoAtaqueDelMonstruo);
+            this.saludJugador -= bajaPuntos;
+            this.turnos.unshift({
+                esJugador:false,
+                text:"El monstruo lastima al jugador por " + bajaPuntos
+            });
+            this.verificarGanador();
         },
 
         calcularHeridas: function (rango) {
-            return 0
-
+            return Math.max(Math.floor(Math.random()* rango[1]) + 1, rango[0]);
         },
         verificarGanador: function () {
+            if(this.saludMonstruo <=0){
+                if(confirm("Ganaste! Jugar de nuevo?")){
+                    this.empezarPartida();
+                }else{
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            }else if ( this.playerHealth <= 0){
+                if(confirm("Perdiste! Jugar de nuevo?")){
+                    this.empezarPartida();
+                }else{
+                    this.hayUnaPartidaEnJuego = false;
+                }
+                return true;
+            }
             return false;
         },
         cssEvento(turno) {
